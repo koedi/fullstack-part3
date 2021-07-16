fc8002c9b8dd5bde9384b8fc6204172bfc498919
koedi <kari.pietikainen@gmail.com>
2021-07-16 17:16:01 +0300

    fix

--- index.js
+++ index.js
@@ -97,9 +97,6 @@ app.post('/api/persons', (req, res) => {
     res.json(newPerson)
 })
 
-
-
-
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
